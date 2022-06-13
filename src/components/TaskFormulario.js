9f754da1fb00bdf01c4eaff149757cea44253028
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2022-06-12 21:08:51 -0300

    projeto finalizado lista de tarefas

--- src/components/TaskFormulario.js
+++ src/components/TaskFormulario.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import '../stylesheets/TaskFormulario.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function TaskFormulario(props){
+
+    const [input,setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+
+    const handleDispatch = e => {
+        e.preventDefault();
+
+        const newTask = {
+            id: uuidv4(),
+            texto: input,
+            completada: false
+        }
+
+        props.onSubmit(newTask);
+    }
+
     return (
-        <form className='task-formulario'>
+        <form className='task-formulario'
+        onSubmit={handleDispatch}>
             <input
             className='task-input'
             type='text'
             placeholder='Escreve uma Task'
             name='texto'
+            onChange={handleChange}
             />
             <button className='task-button'>
                 Adicionar Tarefa
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,18 +5,45 @@ import '../stylesheets/TaskList.css';
 
 function TaskList(){
 
-    const [tasks,setTask] = useState([]);
+    const [tasks,setTasks] = useState([]);
+
+    const addTask = task => {
+       if(task.texto.trim()){
+        task.texto = task.texto.trim();
+        const tasksUpdated = [task,...tasks];
+        setTasks(tasksUpdated);
+       }
+    }
+
+    const eliminarTask = id => {
+        const tasksUpdated = tasks.filter(task => task.id !== id);
+        setTasks(tasksUpdated);
+    }
+
+    const completarTask = id => {
+        const tasksUpdated = tasks.map(task => {
+            if(task.id === id){
+                task.completada = !task.completada;
+            }
+            return task;
+        });
+        setTasks(tasksUpdated);
+    }
 
 
     return (
         <>
-            <TaskFormulario/>
+            <TaskFormulario onSubmit={addTask}/>
             <div className='container-task-list'>
                 {
                     tasks.map((task) => 
                     <Task
+                    key={task.id}
+                    id={task.id}
                     texto={task.texto}
                     completada={task.completada}
+                    completarTask={completarTask}
+                    eliminarTask={eliminarTask}
                     />
                     )
                 }
